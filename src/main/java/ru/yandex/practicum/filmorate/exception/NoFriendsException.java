ebad7dce3fef7926a5ca044812b8eefca8bc3590
aNightcall <111121378+aNightcall@users.noreply.github.com>
2023-03-22 02:01:07 +0200

    Revert "Add database"

--- src/main/java/ru/yandex/practicum/filmorate/exception/NoFriendsException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-import lombok.Data;
-
-@Data
-public class NoFriendsException extends RuntimeException {
-    private final Long parameter;
-}
--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-import lombok.Data;
-
-@Data
-public class NotFoundException extends RuntimeException {
-    private final String parameter;
-}
